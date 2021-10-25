fa6421359095298951298505b6e769c17099d0d3
LucaOcchio99 <occhionorelli.luca99@gmail.com>
2021-10-25 15:29:08 +0200

    end

---
+++ main/main.js
@@ -0,0 +1,15 @@
+//REFERENZE
+const menuTogge = document.querySelector(".header-right > a");
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const closeButton = document.querySelector('.close');
+
+//Mostra menu
+menuToggle.addEventListener('click', function(){
+   hamburgerMenu.classList.add('active');    
+});
+
+//Nascondi manu
+closeButton.addEventListener('click', function(){
+    hamburgerMenu.classList.remove('active');
+})
+
